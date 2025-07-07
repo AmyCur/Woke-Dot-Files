05feb6f2dc8b13f9d9212c5ab9a2fcbdbef487b0
AmyCur <AmyCurry002@proton.me>
2025-07-07 18:21:03 +0100

    commit before i could accidentaly delte my config

--- scripts/theme_swapper/theme_swapper.py
+++ scripts/theme_swapper/theme_swapper.py
@@ -2,20 +2,45 @@ import os
 from subprocess import run
 from shutil import rmtree
 
+
+config_dir = "/home/amy/.config/"
+
 def restart_themes():
     pass
 
 def backup_theme(theme: str):
-    backup_location: str = f"{theme}/{theme}.bak/"
+    backup_location: str = f".{theme}_bak/"
     if(os.path.exists(backup_location)):
         rmtree(backup_location)
 
-    os.system(f"mkdir {theme}/.{theme}_bak/ ; cp -rf {theme}/* {theme}/.{theme}_bak/")
-    os.system(f"rm -r {theme}/.{theme}_bak/.{theme}_bak")
+    os.system(f"mkdir .{theme}_bak/ ; cp -rf {theme}/* .{theme}_bak/")
+    # os.system(f"rm -r {theme}/.{theme}_bak/.{theme}_bak")
+
+def backup_full_config():
+    backup_location: str = f"./.config_bak"
+    if(os.path.exists(backup_location)):
+        rmtree(backup_location)
+
+    os.mkdir(backup_location)
+    os.system(f"cp -rf {config_dir}* {backup_location}")
+
+def overwrite_config(theme: str):
+    for i in (os.listdir(config_dir)):
+        for j in (os.listdir(f"{theme}")):
+            if(i == j):
+                print(f"i: {i} \n j: {j}")
+                rmtree(i)
+                if(os.isdir(j)):
+                    os.system(f"ln -rs {j} {i}")
+                else:
+                    os.system(f"ln {j} {i}")
 
 def update_themes(theme: str):
     print(f"Updating: {theme}")
     backup_theme(theme)
+    backup_full_config()
+    overwrite_config(theme)
+
 
 
 def main():
